dcadb321783423b2f102420cfef291b60b7bda04
Junguilo <Tulcusss@gmail.com>
2023-10-28 14:03:34 -0700

    models updated

--- fitnessApp/models.py
+++ fitnessApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+GOAL_BODY_TYPE = (
+    ("LEAN", "Lean"),
+    ("BUFF", "Buff"),
+    ("CUT", "Cut"),
+    ("BULK", "Bulk")
+)
+
+#Create your models here.
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bmi = models.IntegerField(default = 0)
+    weight = models.IntegerField(default = 0)
+    height = models.IntegerField(default = 0)
+    goalBodyType = models.CharField(
+        maxLength = 20,
+        choices = GOAL_BODY_TYPE,
+        default = 'LEAN'
+    )
+    def str(self):
+        return self.user.username
