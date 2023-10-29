507b73b8ab63d3df90d366c6a93fc672f371e62c
Manuel Samaniego <z23msama@gmail.com>
2023-10-29 05:35:22 -0700

    CSS

--- fitnessApp/forms.py
+++ fitnessApp/forms.py
@@ -20,7 +20,7 @@ class SignupForm(forms.Form):
     
     username = forms.CharField(label='Username', max_length=20, required=True)
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
-    password2 = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(label='Password (again)', widget=forms.PasswordInput, required=True)
 
 class SetupForm(forms.Form):
     def __init__(self, *args, **kwargs):
@@ -31,4 +31,10 @@ class SetupForm(forms.Form):
     
     weight = forms.IntegerField(label='Weight (pounds)', required=True)
     height = forms.IntegerField(label='Height (inches)', required=True)
-    goal = forms.ChoiceField(label='Goal', choices=GOAL_BODY_TYPE, required=True)
+    goal = forms.ChoiceField(label='Goal/Ideal Body Type', choices=GOAL_BODY_TYPE, required=True)
+
+class UpdateForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(UpdateForm, self).__init__(*args, **kwargs)
+    
+    weight = forms.IntegerField(label='Weight (pounds, negative if you lost weight)', required=True)
